28bb224d8611ec71e4f0185481a035fc37fdc641
AlexJM <103997784+AlexJM14@users.noreply.github.com>
2024-05-22 14:01:22 +0200

    Initial Commit

---
+++ Sprint4/Sprint4App.swift
@@ -0,0 +1,17 @@
+//
+//  Sprint4App.swift
+//  Sprint4
+//
+//  Created by Alex Jumbo on 22/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Sprint4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
